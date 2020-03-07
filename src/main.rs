e75fabbe4c67466620a52e17eb18f18173614f64
stvsmth <stvsmth@gmail.com>
2020-03-07 10:31:12 -0700

    Refactor arg parsing to function.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
-    let mut file = File::open(filename).expect("Error opening file.");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Error reading file.");
+    let contents = fs::read_to_string(filename).expect("Error reading file.");
     println!("Searching for: {}", query);
     println!("In file: {}", filename);
     println!("First 30 chars: {} ", &contents[..30]);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
