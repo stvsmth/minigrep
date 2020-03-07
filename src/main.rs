f6e927e53518c2d3a598d505cd6bfa752f76ff33
stvsmth <stvsmth@gmail.com>
2020-03-07 10:10:50 -0700

    First stab at reading contents.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    let mut file = File::open(filename).expect("Error opening file.");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Error reading file.");
+    println!("Searching for: {}", query);
+    println!("In file: {}", filename);
+    println!("First 30 chars: {} ", &contents[..30]);
 }
