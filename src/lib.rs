5c87373024ed6454ad64a26944141fc457dcc99d
stvsmth <stvsmth@gmail.com>
2020-03-11 20:34:35 -0600

    Use iterator for arg parsing.

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); // skip over app name
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query param"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename param"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {
             query,
